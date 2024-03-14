4707d498bd902be55615df6008f61277cbee3972
NyxControl <157659323+NyxControl@users.noreply.github.com>
2024-03-14 14:37:12 +0100

    Create blacklist.js

---
+++ blacklist.js
@@ -0,0 +1,19 @@
+{
+    "enemy_player": [
+        "He6ecHblu",
+        "Guccimane"
+    ],
+    "enemy_clan": [
+        "99",
+        "NL",
+        "sRev",
+        "R8",
+        "DEO"
+    ],
+    "ally_player": [
+        "."
+    ],
+    "ally_clan": [
+        "LGNDS"
+    ]
+}
